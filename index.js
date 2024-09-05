91667cf3311ccbf548f4abe53a043bcf2b6d2134
Thamarai <tmanalan710@gmail.com>
2024-09-05 13:10:33 +0530

    using the path

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express();
+const path=require("path")
+
 
 app.get('/', (req, res)=>{
-    res.send('hey')
+    const pathuse=path.join(__dirname,"public/index.html")
+    res.sendFile(pathuse)
+    
 })
-
+app.post('/auth',(req, res)=>{
+    res.send(data)
+})
 app.listen(3000,()=>{
     console.log('app listening in the port 3000');
     
