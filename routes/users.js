4470d53f577214d3009a38c8cff8e63a042dd596
Thamarai <tmanalan710@gmail.com>
2024-09-05 21:11:52 +0530

    get and delete functionalitys are working

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,33 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const {v4: uuidv4} = require('uuid')
+const { v4: uuidv4 } = require("uuid");
 
-let users = []
+let users = [];
+
+router.get("/", (req, res) => {
+  res.send(users);
+});
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push({ ...user, id: uuidv4() });
+
+  res.send("the data was added");
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
 
 
-
-router.get('/',(req, res)=>{
-    res.send(users);
-})
-router.post('/', (req, res)=>{
-    const user = req.body;
-    users.push({ ...user, id: uuidv4() });
-
-    res.send('the data was added');
-})
-
-router.get('/:id', (req, res)=>{
-    const {id} = req.params;
-
-    const foundUser = users.find((user)=>user.id === id)
-
-    res.send(foundUser)
-})
+  res.send(`${id} deleted successfully`);
+});
 
 module.exports = router;
